64b6a5627d71382d5ca7cb7e1bf3c5bfaaf671f6
Brian Fontillas <BrianKFontillas@gmail.com>
2024-04-22 00:54:59 -0700

    more middlewares

--- contacts.js
+++ contacts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const ALPHA = "abcdefghijklmnopqrstuvwxyz";
+const NUMBERS = "0123456789";
 
 let contactData = [
   {
@@ -41,6 +43,47 @@ const sortContacts = contacts => {
   });
 };
 
+const isUnique = (firstName, lastName) => {
+  for (let contact of contactData) {
+    if (contact.firstName === firstName &&
+        contact.lastName === lastName) return false;
+  }
+
+  return true;
+
+}
+
+const containsNonAlpha = str => {
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i ++) {
+    if (!ALPHA.includes(str[i])) return true;
+  }
+
+  return false;
+}
+
+const isUSPhoneNumber = phoneNumber => {
+  //check sets
+  let numsArray = phoneNumber.split('-');
+  if (numsArray[0].length !== 3 ||
+      numsArray[1].length !== 3 ||
+      numsArray[2].length !== 4) return false;
+  //check length
+  let onlyNums = numsArray.join('');
+  if (onlyNums.length !== 10) return false;
+
+  //check characters
+  for (let i = 0; i < onlyNums.length; i++) {
+    if (!NUMBERS.includes(onlyNums[i])) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -62,11 +105,96 @@ app.get("/contacts/new-contact", (req, res) => {
   res.render("new-contact");
 });
 
-app.post("/contacts/new-contact", (req, res) => {
-  contactData.push({ ...req.body });
+app.post("/contacts/new-contact",
+  (req, res, next) => {
+    res.locals.errorMessages = [];
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.firstName.length === 0) {
+      res.locals.errorMessages.push("First name is required.");
+    }
 
-  res.redirect("/contacts");
-});
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.lastName.length === 0) {
+      res.locals.errorMessages.push("Last name is required.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.phoneNumber.length === 0) {
+      res.locals.errorMessages.push("Phone number is required.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.firstName.length > 25) {
+      res.locals.errorMessages.push("First name exceeds character limit of 25");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.lastName.length > 25) {
+      res.locals.errorMessages.push("Last name exceeds character limit of 25");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (containsNonAlpha(req.body.firstName)) {
+      res.locals.errorMessages.push("Please enter valid first name");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (containsNonAlpha(req.body.lastName)) {
+      res.locals.errorMessages.push("Please enter valid last name");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (!isUSPhoneNumber(req.body.phoneNumber)) {
+      res.locals.errorMessages.push("Please enter valid phone number in the correct format");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (!isUnique(req.body.firstName, req.body.lastName)) {
+      res.locals.errorMessages.push("Contact already exists");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (res.locals.errorMessages.length > 0) {
+      res.render("new-contact", {
+        errorMessages: res.locals.errorMessages,
+        firstName: res.locals.firstName,
+        lastName: res.locals.lastName,
+        phoneNumber: res.locals.phoneNumber,
+      });
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    contactData.push({
+      firstName: req.body.firstName.trim(),
+      lastName: req.body.lastName.trim(),
+      phoneNumber: req.body.phoneNumber.trim(),
+    });
+
+    res.redirect("/contacts");
+  }
+);
 
 app.listen(3000, "localhost", () => {
   console.log("Listening to port 3000.");
